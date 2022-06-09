3e8775b6b366285222427610f4c9b25aeb4b13b5
wannalearnit <m.g.aminov@yandex.ru>
2022-06-09 13:52:44 +0300

    fucking github

---
+++ main.py
@@ -0,0 +1,63 @@
+import logging
+import sqlite3
+import time
+
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+
+API_TOKEN = '5451160025:AAGFakoc_h0rKWuWyqPFlJ9EdjV_wj6xK48'
+base = sqlite3.connect('TimeBase.db')
+cur = base.cursor()
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    with open("list_of_commands.txt", "r") as file:
+        list_of_commands = file.read()
+    await message.reply(list_of_commands)
+
+
+
+@dp.message_handler(commands=['set'])
+async def set_timer(message: types.Message):
+    line = message.text[5:]
+    if line.isdigit():
+        user_id = message.from_user.id
+        vrem = int(time.time()) + int(line) * 60
+        cur.execute("INSERT INTO users_calls VALUES (?, ?)", [user_id, vrem])
+        base.commit()
+        await message.answer('таймер на ' + line + ' минут установлен' + str(user_id))
+    else:
+        await message.answer('Syntax.Error')
+
+
+
+async def spamer(timer):
+    while True:
+        kp = cur.execute("SELECT * FROM users_calls ORDER BY timestamp")
+        for row in kp:
+            user_id = row[0]
+            tstamp = row[1]
+            if tstamp < int(time.time()):
+                await bot.send_message(user_id, 'time is gone')
+                cur.execute("DELETE FROM users_calls WHERE (iser_id = ?) AND (timestamp = ?)", [user_id, tstamp])
+                base.commit()
+            else:
+                break
+        await asyncio.sleep(timer)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(spamer(1))
+    executor.start_polling(dp, skip_updates=True)
+
+
+
